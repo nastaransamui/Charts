b62d1d57ae7b5d05df93c24ccb235f1ec53acde9
nastaransamui <nastaran.samui@gmail.com>
2021-03-17 09:37:27 +0700

    callBack

--- pages/api/auth/error.js
+++ pages/api/auth/error.js
@@ -2,10 +2,11 @@
 export default async (req, res) => {
   console.log(process.env)
   console.log(req.cookies)
-  if (process.env.NEXTAUTH_URL === 'http://localhost:3000') {
-    res.redirect(`${req.cookies[`next-auth.callback-url`]}?error=` + req.query.error);
-  } else {
-    res.redirect(`${req.cookies[`__Secure-next-auth.callback-url`]}?error=` + req.query.error);
-  }
+  res.redirect(`${process.env.NEXTAUTH_URL}/login?error=` + req.query.error)
+//   if (process.env.NEXTAUTH_URL === 'http://localhost:3000') {
+//     res.redirect(`${req.cookies[`next-auth.callback-url`]}?error=` + req.query.error);
+//   } else {
+//     res.redirect(`${req.cookies[`__Secure-next-auth.callback-url`]}?error=` + req.query.error);
+//   }
     
   }
